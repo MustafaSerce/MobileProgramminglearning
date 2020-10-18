ba99fdf9cb136451e527728aca2bf8d938afa687
MustafaSerce <73030888+MustafaSerce@users.noreply.github.com>
2020-10-19 00:01:15 +0300

    My 1.1 Code and App Design

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myfirstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
 
     TextView helloworldtext;
     Button modifybutton;
+    Button changecolorbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,12 +20,16 @@ public class MainActivity extends AppCompatActivity {
 
         helloworldtext = findViewById(R.id.textView);
         modifybutton = findViewById(R.id.btn_modfy);
+        changecolorbtn = findViewById(R.id.changecolor);
 
         modifybutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 helloworldtext.setText("My First Code ! ");
+                helloworldtext.setText("My 1.1 code");
             }
+
         });
+
     }
 }
