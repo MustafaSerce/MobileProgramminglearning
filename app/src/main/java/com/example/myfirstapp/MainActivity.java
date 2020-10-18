613906cb3c1935173e363c4b0b4bfe62572313be
MustafaSerce <73030888+MustafaSerce@users.noreply.github.com>
2020-10-18 05:11:26 +0300

    My First Code and App Design

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.myfirstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView helloworldtext;
+    Button modifybutton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        helloworldtext = findViewById(R.id.textView);
+        modifybutton = findViewById(R.id.btn_modfy);
+
+        modifybutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                helloworldtext.setText("My First Code ! ");
+            }
+        });
     }
 }
